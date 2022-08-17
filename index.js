7c3c797538b535ed61c9bae987bbe969f8468703
Sierra Nutt <sierra.nutt@outlook.com>
2022-08-17 10:51:30 -0600

    finished functionality for now

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const fs = require('fs');
 const promptUser = () => {
   return inquirer.prompt([
     {
+    type: 'checkbox',
+    name: 'title',
+    message: 'What is your job title?',
+    choices: [
+        'Manager', 'Engineer', 'Intern',
+    ],
+      },
+    {
       type: 'input',
       name: 'name',
       message: 'What is your name?',
@@ -28,7 +36,43 @@ const promptUser = () => {
   ]);
 };
 
-const generateHTML = ({ name, location, github, email }) =>
+const addUser = () => {
+    return inquirer.prompt([
+    {
+        type: 'checkbox',
+        name: 'addUser',
+        message: 'Would you like to add a team member?',
+        choices: [
+            'Yes', 'No',
+        ],
+            },
+      {
+      type: 'checkbox',
+      name: 'title1',
+      message: 'What is your job title?',
+      choices: [
+          'Engineer', 'Intern',
+      ],
+        },
+      {
+        type: 'input',
+        name: 'name1',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'email1',
+        message: 'What is your email?',
+      },
+      {
+        type: 'input',
+        name: 'github1',
+        message: 'Enter your GitHub Username',
+      },
+    ]);
+  };
+
+const generateHTML = ({title, name, location, github, email}) =>
   `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -42,44 +86,44 @@ const generateHTML = ({ name, location, github, email }) =>
     <div class="container col-4">
       <h1 class="display-4">My Team</h1>
       <div class="card col">
-          <h3>${name}<span class="badge badge-secondary">Contact Me</span></h3>
+          <h3>${name}</h3>
+          <p><span class="badge badge-secondary">${title}</span></p>
           <ul class="list-group">
           <li class="list-group-item">ID: ${name}</li>
-          <li class="list-group-item">GitHub: ${github}</li>
-          <li class="list-group-item">Email: ${email}</li>
+          <li class="list-group-item">GitHub: <a href="https://github.com/${github}">${github}</a></li>
+          <li class="list-group-item">Email: <a href = "mailto: ${email}">${email}</a></li>
           <li class="list-group-item">Office Number: ${location}</li>
           </ul>
-    </div>
-    <div class="card col">
-        <h3>${name}<span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-        <li class="list-group-item">ID: ${name}</li>
-        <li class="list-group-item">GitHub: <a href="https://github.com/" + ${github}></a>${github}</li>
-        <li class="list-group-item">Email: ${email}</li>
-        <li class="list-group-item">Office Number: ${location}</li>
-        </ul>
-    </div>
-    <div class="card col">
-        <h3>${name}<span class="badge badge-secondary">Contact Me</span></h3>
-        <ul class="list-group">
-        <li class="list-group-item">ID: ${name}</li>
-        <li class="list-group-item">GitHub: ${github}</li>
-        <li class="list-group-item">Email: ${email}</li>
-        <li class="list-group-item">Office Number: ${location}</li>
-        </ul>
-    </div>
+      </div>
     </div>
     </div>  
-  </body>
+  </body> 
   </html>`;
+
+  const appendHTML = ({title1, name1, github1, email1 }) =>
+  ` <div class="container col-4">   
+        <div class="card col">
+            <h3>${name1}</h3>
+            <p><span class="badge badge-secondary">${title1}</span></p>
+            <ul class="list-group">
+            <li class="list-group-item">ID: ${name1}</li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${github1}">${github1}</a></li>
+            <li class="list-group-item">Email: <a href = "mailto: ${email1}">${email1}</a></li>
+            </ul>
+      </div>
+    </div>`;
 
 // Bonus using writeFileSync as a promise
 const init = () => {
   promptUser()
     // Use writeFileSync method to use promises instead of a callback function
     .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
+    .then(() => console.log('Successfully created index.html'))
+    .then(() => addUser())
+    .then((answers) => fs.appendFileSync('index.html', appendHTML(answers)))
+    .then(() => console.log('Successfully appended to index.html'))
     .catch((err) => console.error(err));
 };
 
 init();
+
